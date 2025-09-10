ef6949484e3c5117e94b251869616d7cd0b91953
Kelan Rafferty <KelanRafferty@PMS.local>
2025-09-10 11:25:53 +0100

    fixed login bug + restructured DB

--- EmployeeTrainingTracker/obj/Debug/net8.0-windows/EmployeeTrainingTracker.AssemblyInfo.cs
+++ EmployeeTrainingTracker/obj/Debug/net8.0-windows/EmployeeTrainingTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeTrainingTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73f1d687a35bdb394b87b7fa8ee83874d81f87b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5187a2b7be4dce5f0ed918bd3195290c3ba249ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeTrainingTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeTrainingTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
